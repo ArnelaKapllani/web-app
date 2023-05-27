984089ac5c050216b389523b7920b882cf824c7b
arnelakapllani <arnela.kapllani@web.de>
2023-05-27 22:53:15 +0200

    add new html file

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,15 @@
 console.clear();
 
-const showAnswer = document.querySelector('[data-js="button-show-answer"]');
-showAnswer.addEventListener("click", () => {
-  showAnswer.classList.add("openpopup");
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+bookmarkButton.addEventListener("click", function onClick(event) {
+  bookmarkButton.classList.toggle("color");
+});
+
+const answerButton = document.querySelector('[data-js="button-show-answer"]');
+const answer = document.querySelector('[data-js="hidden-answer"]');
+
+answerButton.addEventListener("click", function (buttonText) {
+  buttonText.target.classList.toggle("changeButtonText");
+  answer.classList.toggle("hidden");
 });
